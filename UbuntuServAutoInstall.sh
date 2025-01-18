7d54559c94d8838c26d1fb13bd85184e8b0fb8be
Krzysztof M <156579182+kamile320@users.noreply.github.com>
2025-01-18 14:49:00 +0100

    Updated to v1.4

--- UbuntuServAutoInstall.sh
+++ UbuntuServAutoInstall.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
-#PacakageInstaller v1.3
+#PacakageInstaller v1.4
+echo "Instalator Pakietów v1.4"
+echo "Program pobierze wszystkie wymagane pakiety do zadań egzaminacyjnych INF.02 (Ubuntu Server)"
+sleep 3
+read -p "Type anything to continue (need root privileges)"
+
 
 #Update Repo
 sudo apt update -y
 #Pakiety
 sudo apt install -y mc lynx net-tools traceroute nmap hwinfo atop duplicity libpam-pwquality cifs-utils smartmontools nano file
 #Dodatkowe
-sudo apt install -y git bashtop xsysinfo neofetch hwinfo lshw lshw-gtk tree g++ network-manager
+sudo apt install -y git xsysinfo neofetch hwinfo lshw lshw-gtk tree g++ network-manager
 #Serwery
 sudo apt install -y apache2 samba vsftpd isc-dhcp-server
 #Serwery - DNS
 sudo apt install -y bind9 bind9utils bind9-doc dnsutils
-#Upgrade
-sudo apt upgrade -y
 #Apache
 sudo a2enmod userdir
+sleep 2
+#Dodatkowe2
+echo "Próba pobrania pakietów które mogą nie występywać w każdym repozytorium"
+sleep 2
+sudo apt install -y bashtop
+sudo apt install -y btop
+sleep 2
+#Upgrade
+echo "Starting system upgrade..."
+sleep 2
+sudo apt upgrade -y
+sleep 1
+echo "Running apt autoremove..."
+sleep 2
+sudo apt autoremove -y
+sleep 1
+
 #Reboot
+read -p "Zrestartować komputer? (Ctrl-C to cancel)"
 sudo reboot now
 
 #INFO
