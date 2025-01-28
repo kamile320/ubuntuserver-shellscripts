825ed9fafa30cc2e5e1904f3087776ef265b1214
Krzysztof M <156579182+kamile320@users.noreply.github.com>
2025-01-28 18:25:27 +0100

    Created DesktopInstall.sh
    Created DesktopInstall.sh that installs useful programs and packages for Ubuntu Desktop

---
+++ DesktopInstall.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#DesktopInstall v1.0
+echo "Instalator Pakietów v1.0 (Ubuntu Desktop)"
+echo "Skrypt pobierze przydatne pakiety i programy dla Ubuntu Desktop"
+sleep 3
+read -p "Type anything to continue (need root privileges)"
+
+#Update Repo
+sudo apt update -y
+#Install
+sudo apt install neofetch git mc nano lynx net-tools network-manager traceroute nmap hwinfo atop lshw lshw-gtk tree g++ gparted
+
+#Bashtop
+#atop, btop i bashtop to podobne programy (Szczególnie btop i bashtop)
+#Służą do monitorowania procesów itp.
+#Jeśli chcesz pobrać jeden z poniższych programów, odhashtaguj je:
+#sudo apt install -y btop
+#sudo apt install -y bashtop
+
+#Upgrade
+echo "Starting system upgrade..."
+sleep 2
+sudo apt upgrade -y
+sleep 1
+echo "Running apt autoremove..."
+sleep 2
+sudo apt autoremove -y
+sleep 1
+
+#Flatpak
+echo "Czy chcesz pobrać flatpak i przydatne programy?"
+echo "Następujące pakiety/programy zostaną zainstalowane:"
+echo "(APT) flatpak"
+echo "(Flatpak) VisualStudioCode, GithubDesktop, Discord"
+read -p "Type anything to continue (Ctrl-C to cancel)"
+
+#Flatpak-Install
+sudo flatpak install com.visualstudio.code
+sudo flatpak install io.github.shiftey.Desktop
+sudo flatpak install com.discordapp.Discord
+
+#Reboot
+read -p "Zrestartować komputer? (Ctrl-C to cancel)"
+sudo reboot now
